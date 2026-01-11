43d727f6edc6f691e0708359495ab53fe58540cb
Piyush_Bansal9 <piyushbansal130p@gmail.com>
2026-01-11 22:01:45 +0530

    Payments-Using-Polar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -6,6 +6,7 @@ import Link from "next/link"
 import Image from "next/image"
 import { usePathname, useRouter } from "next/navigation"
 import { authClient } from "@/lib/auth-client"
+import { useHasActiveSubscription } from "@/features/subscriptions/hooks/use-subscription"
 
 
 const menuItems = [
@@ -30,9 +31,10 @@ const menuItems = [
         ]
     }
     ]
-    const AppSidebar = () => {
+export const AppSidebar = () => {
     const router = useRouter()
     const pathname = usePathname()
+    const { hasActiveSubscription, isLoading } = useHasActiveSubscription();
     return (
         <Sidebar collapsible="icon">
         <SidebarHeader>
@@ -77,22 +79,24 @@ const menuItems = [
         <SidebarFooter>
             <SidebarMenu>
 
-            <SidebarMenuItem>
-                <SidebarMenuButton
-                tooltip="Upgrade to Pro"
-                className="gap-x-4 h-10 px-4"
-                onClick={() => { }}
-                >
-                <StarIcon className="h-4 w-4" />
-                <span>Upgrade to Pro</span>
-                </SidebarMenuButton>
-            </SidebarMenuItem>
+            {!hasActiveSubscription && !isLoading && (
+                <SidebarMenuItem>
+                    <SidebarMenuButton
+                    tooltip="Upgrade to Pro"
+                    className="gap-x-4 h-10 px-4"
+                    onClick={() => {authClient.checkout({ slug: "pro" })}}
+                    >
+                        <StarIcon className="h-4 w-4" />
+                        <span>Upgrade to Pro</span>
+                    </SidebarMenuButton>
+                </SidebarMenuItem>
+            )}
 
             <SidebarMenuItem>
                 <SidebarMenuButton
                 tooltip="Billing Portal"
                 className="gap-x-4 h-10 px-4"
-                onClick={() => { }}
+                onClick={() => { authClient.customer.portal() }}
                 >
                 <CreditCardIcon className="h-4 w-4" />
                 <span>Billing Portal</span>
@@ -120,5 +124,3 @@ const menuItems = [
         </Sidebar>
     )
 }
-
-export default AppSidebar
