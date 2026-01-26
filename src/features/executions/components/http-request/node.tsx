f8d9c08a2e3fa8d5556347a278313031e150872c
Piyush_Bansal9 <piyushbansal130p@gmail.com>
2026-01-26 22:12:00 +0530

    NodesExecution-ExecutePipeline

--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -3,13 +3,12 @@ import {useReactFlow, type Node, type NodeProps} from "@xyflow/react";
 import { GlobeIcon } from "lucide-react";
 import {memo, useState} from "react";
 import { BaseExecutionNode } from "../base-execution-node";
-import { FormType, HTTPRequestDialog } from "./dialog";
+import { HTTPRequestFormValues, HTTPRequestDialog } from "./dialog";
 
 type HttpRequestNodeData = {
     endpoint?: string;
     method?: "GET" | "PUT" | "POST" | "PUT" | "PATCH" | "DELETE";
     body?: string;
-    [key: string]: unknown
 }
 
 type HttpRequestNodeType = Node<HttpRequestNodeData>
@@ -26,16 +25,14 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
 
     const {setNodes} = useReactFlow();
 
-    const handleSubmit = (values: FormType) => {
+    const handleSubmit = (values: HTTPRequestFormValues) => {
         setNodes((nodes) => nodes.map((node) => {
             if(node.id === props.id) {
                 return {
                     ...node,
                     data: {
                         ...node.data,
-                        endpoint: values.endpoint,
-                        method: values.method,
-                        body: values.body
+                        ...values
                     }
                 }
             }
@@ -49,9 +46,7 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
                 open={open} 
                 onOpenChange={setOpen}
                 onSubmit={handleSubmit}
-                defaultEndpoint={nodeData.endpoint}
-                defaultMethod={nodeData.method}
-                defaultBody={nodeData.body}
+                defaultValues={nodeData}
             />
             <BaseExecutionNode
                 {...props}
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,49 +1,50 @@
+import { NonRetriableError } from "inngest";
 import { inngest } from "./client";
-import { createGoogleGenerativeAI } from "@ai-sdk/google";
-import { createOpenAI }  from "@ai-sdk/openai"
-import { generateText } from "ai";
+import prisma from "@/lib/db";
+import { topologicalSort } from "./utils";
+import { NodeType } from "@/generated/prisma";
+import { getExecutor } from "@/features/executions/lib/executor-registry";
 
-const google = createGoogleGenerativeAI();
-const openai = createOpenAI();
-
-export const execute = inngest.createFunction(
-    { id: "execute-ai" },
-    { event: "execute/ai" },
+export const executeWorkflow = inngest.createFunction(
+    { id: "execute-workflow" },
+    { event: "workflows/execute.workflow" },
     async ({ event, step }) => {
-        await step.sleep("pretend", "5s")
-        const { steps: GeminiSteps } = await step.ai.wrap(
-            "gemini-generate-text",
-            generateText,
-            {
-                model: google("gemini-2.5-flash"),
-                system: "Your are a helpful assistant.",
-                prompt: "What is 2+2?",
-                experimental_telemetry: {
-                    isEnabled: true,
-                    recordInputs: true,
-                    recordOutputs: true,
-                },
-            }
-        )
+        const workflowId = event.data.workflowId;
 
-        const { steps: OpenAiStpes } = await step.ai.wrap(
-            "openai-generate-text",
-            generateText,
-            {
-                model: openai("gpt-4"),
-                system: "Your are a helpful assistant.",
-                prompt: "What is 2+2?",
-                experimental_telemetry: {
-                    isEnabled: true,
-                    recordInputs: true,
-                    recordOutputs: true,
+        if(!workflowId) {
+            throw new NonRetriableError("Workflow ID is missing.");
+        }
+
+        const sortedNodes = await step.run("prepare-workflow", async() => {
+            const workflow = await prisma.workflow.findUniqueOrThrow({
+                where: {
+                    id: workflowId,
                 },
-            }
-        )
-        
-        return {
-            GeminiSteps,
-            OpenAiStpes
+                include: {
+                    nodes: true,
+                    connections: true,
+                }
+            });
+
+            return topologicalSort(workflow.nodes, workflow.connections);
+        });
+
+        //Initialize the context with any initial data from the trigger.
+        let context = event.data.initialData || {};
+
+        for (const node of sortedNodes) {
+            const executor = getExecutor(node.type as NodeType)
+            context = await executor({
+                data: node.data as Record<string, unknown>,
+                nodeId: node.id,
+                context,
+                step,
+            });
+        }
+
+        return { 
+            workflowId,
+            context
         };
     },  
 );
