c9f17ebce6f30fa8adfbdd7ed3b6144c0b318fa9
Piyush_Bansal9 <piyushbansal130p@gmail.com>
2026-02-10 23:56:10 +0530

    Add Gemini AI Node

--- src/config/node-components.ts
+++ src/config/node-components.ts
@@ -1,4 +1,5 @@
 import { InitialNode } from "@/components/initial-node";
+import { GeminiNode } from "@/features/executions/components/gemini/node";
 import { HttpRequestNode } from "@/features/executions/components/http-request/node";
 import { GoogleFormTrigger } from "@/features/triggers/components/google-form-trigger/node";
 import { ManulTriggerNode } from "@/features/triggers/components/manual-trigger/node";
@@ -13,6 +14,7 @@ export const nodeComponents = {
     [NodeType.MANUAL_TRIGGER]: ManulTriggerNode,
     [NodeType.GOOGLE_FORM_TRIGGER]: GoogleFormTrigger,
     [NodeType.STRIPE_TRIGGER]: StripeTriggerNode,
+    [NodeType.GEMINI]: GeminiNode
 } as const satisfies NodeTypes
 
 export type RegisteredNodeType = keyof typeof nodeComponents
